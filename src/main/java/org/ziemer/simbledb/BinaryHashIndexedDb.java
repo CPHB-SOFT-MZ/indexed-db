c5cf3a14b2140e96833ce74f2a67bd569be5b18d
Mikkel Ziemer <mzhjensen@gmail.com>
2018-02-05 12:10:37 +0100

    Removed index from DB file

--- src/main/java/org/ziemer/simbledb/BinaryHashIndexedDb.java
+++ src/main/java/org/ziemer/simbledb/BinaryHashIndexedDb.java
@@ -33,7 +33,7 @@ public class BinaryHashIndexedDb implements IndexedDb {
 
     @Override
     public void write(String key, String value) throws IOException {
-        String data = key + "," + value;
+        String data = value;
         byte[] byteArray = data.getBytes(StandardCharsets.UTF_8);
 
         // Save the place we are going to save our data, in our HashMap
@@ -72,7 +72,7 @@ public class BinaryHashIndexedDb implements IndexedDb {
             data.append((char) Integer.parseInt(line, 2));
         }
 
-        return data.toString().replaceAll(".*,", "");
+        return data.toString();
     }
 
     @Override
